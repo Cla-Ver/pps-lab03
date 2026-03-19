3cbb7ebd7d9e975f047a11e7860de983e3e4baee
Claudio Verazza <claudio.verazza@studio.unibo.it>
2026-03-19 17:43:54 +0100

    Done list courses and foldLeft

--- src/main/scala/it/unibo/pps/u03/Person.scala
+++ src/main/scala/it/unibo/pps/u03/Person.scala
@@ -1,6 +1,9 @@
 package it.unibo.pps.u03
 
-import u03.Sequences.*
+import u03.Sequences.Sequence
+import u03.Sequences.Sequence.*
+
+import scala.annotation.tailrec
 
 enum Person:
   case Student(name: String, year: Int)
@@ -15,9 +18,15 @@ object Person:
     case Student(_, _) => true
     case _ => false
 
-  import u03.Sequences
-  //def teachersCourses(persons: Sequence[Person]): Sequence[String] = persons.
+  def course(p: Person): String = p match
+    case Teacher(_, course) => course
 
+  def teachersCourses(persons: Sequence[Person]): Sequence[String] = map(filter(persons)(person => !isStudent(person)))(course)
+
+  @tailrec
+  def foldLeft[A, B](sequence: Sequence[A])(initialValue: B)(operator: (B, A) => B): B = sequence match
+    case Nil() => initialValue
+    case Cons(h, t) => foldLeft(t)(operator(initialValue, h))(operator)
 
 
 
---
+++ src/test/scala/it/unibo/pps/u03/PersonListTest.scala
@@ -0,0 +1,20 @@
+package it.unibo.pps.u03
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import u03.Sequences.Sequence
+import u03.Sequences.Sequence.*
+
+class PersonListTest:
+  import Person.*
+  val sequence: Sequence[Person] = Cons(Student("Mario", 2000), Cons(Teacher("Mirko", "PPS"), Cons(Teacher("Aguzzi", "PPS"), Nil())))
+  val intSequence: Sequence[Int] = Cons(3, Cons(7, Cons(1, Cons(5, Nil()))))
+
+  @Test def teacherCoursesShouldReturnCourses(): Unit =
+    assertEquals(Cons("PPS", Cons("PPS", Nil())), teachersCourses(sequence))
+
+  @Test def foldLeftShouldAccumulateValues(): Unit =
+    assertEquals(-16, foldLeft(intSequence)(0)(_ - _))
+
+  @Test def foldLeftShouldConcatenateNumbers(): Unit =
+    assertEquals("3715", foldLeft(intSequence)("")(_ + _))
