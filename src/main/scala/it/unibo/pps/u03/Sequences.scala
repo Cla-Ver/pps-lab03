5a8b99655ea0953b83968e6c935cc8044e262bcd
Claudio Verazza <claudio.verazza@studio.unibo.it>
2026-03-24 19:36:48 +0100

    Simplified zip

--- src/main/scala/it/unibo/pps/u03/Sequences.scala
+++ src/main/scala/it/unibo/pps/u03/Sequences.scala
@@ -48,10 +48,8 @@ object Sequences: // Essentially, generic linkedlists
      * E.g., [], [] => []
      */
     def zip[A, B](first: Sequence[A], second: Sequence[B]): Sequence[(A, B)] = (first, second) match
-      case (_, Nil()) => Nil()
-      case (Nil(), _) => Nil()
       case (Cons(h1, t1), Cons(h2, t2)) => Cons((h1, h2), zip(t1, t2))
-
+      case _ => Nil()
 
     /*
      * Concatenate two sequences
